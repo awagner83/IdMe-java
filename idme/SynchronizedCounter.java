c0bd8b3729f7bce603d1595e48f8da8fcc0b2602
Adam Wagner <awagner83@gmail.com>
2011-11-20 09:55:38 -0500

    added docs for SynchronizedCounter class.

--- idme/SynchronizedCounter.java
+++ idme/SynchronizedCounter.java
@@ -1,5 +1,17 @@
 package idme;
 
+/**
+ * Thread-safe incrementing counter.
+ *
+ * NOTE - I'm not entirely sure that I need this.  '++foo' may atomic,
+ * but I figure since this may one day be a synchronized-hash, it'll
+ * matter more then.
+ *
+ * Also, this'll eventually fail when numbers get too big, since I'm
+ * just using `ints`.
+ *
+ * @author Adam Wagner
+ */
 public class SynchronizedCounter {
 
     private int count = 0;
